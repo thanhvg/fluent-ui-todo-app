19a8986adf58f729a5a5a3479b88d5ab62c830af
Thanh Vuong <thanhvg@gmail.com>
2020-05-22 13:02:42 -0600

    00

---
+++ src/components/Todo.scss.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+import { loadStyles } from '@microsoft/load-themed-styles';
+loadStyles([{"rawString":".todo_f0719dd6{padding:28px 40px 60px;max-width:640px;margin:0 auto;border:2px solid #f3f2f1;font-size:14px;font-weight:400}.todo_f0719dd6 .topRow_f0719dd6{position:relative}.todo_f0719dd6 .todoHeading_f0719dd6{display:inline-block}.todo_f0719dd6 .todoPivot_f0719dd6{padding-top:24px}.todo_f0719dd6 .todoForm_f0719dd6{display:table-row}.todo_f0719dd6 .todoForm_f0719dd6 .textField_f0719dd6{display:table-cell;width:100%;vertical-align:top}.todo_f0719dd6 .todoForm_f0719dd6 .addButton_f0719dd6{display:table-cell;white-space:nowrap}[dir=ltr] .todo_f0719dd6 .todoForm_f0719dd6 .addButton_f0719dd6{margin-left:16px}[dir=rtl] .todo_f0719dd6 .todoForm_f0719dd6 .addButton_f0719dd6{margin-right:16px}.todo_f0719dd6 .todoList_f0719dd6{margin-top:20px;border-top:1px solid #edebe9}.todo_f0719dd6 .todoList_f0719dd6 .todoItem_f0719dd6{border:1px solid #edebe9;border-top:none}.todo_f0719dd6 .todoList_f0719dd6 .todoItem_f0719dd6 .itemTaskRow_f0719dd6{padding:16px 20px}.todo_f0719dd6 .todoList_f0719dd6 .todoItem_f0719dd6 .itemTaskRow_f0719dd6 .deleteButton_f0719dd6{position:absolute;top:50%;-webkit-transform:translateY(-50%);transform:translateY(-50%);right:0}[dir=ltr] .todo_f0719dd6 .todoList_f0719dd6 .todoItem_f0719dd6 .itemTaskRow_f0719dd6 .deleteButton_f0719dd6{margin-right:16px}[dir=rtl] .todo_f0719dd6 .todoList_f0719dd6 .todoItem_f0719dd6 .itemTaskRow_f0719dd6 .deleteButton_f0719dd6{margin-left:16px}.todo_f0719dd6 .todoList_f0719dd6 .todoItem_f0719dd6 .itemTaskRow_f0719dd6 .deleteButton_f0719dd6:focus,.todo_f0719dd6 .todoList_f0719dd6 .todoItem_f0719dd6 .itemTaskRow_f0719dd6 .deleteButton_f0719dd6:hover{color:#0078d4}.todo_f0719dd6 .todoList_f0719dd6 .todoItem_f0719dd6.isHidden_f0719dd6{display:none}.todo_f0719dd6 .todoList_f0719dd6 .todoItem_f0719dd6.isCompleted_f0719dd6{border-bottom:1px solid #fff;border-left:1px solid #f3f2f1;border-right:1px solid #f3f2f1;background-color:#f3f2f1}.todo_f0719dd6 .workingOnItSpinner_f0719dd6{display:inline-block;position:absolute;top:0;bottom:0;margin:auto;height:33.33333%;line-height:1.5em;padding:0 24px}.todo_f0719dd6 .fetchingTasksSpinner_f0719dd6{display:-webkit-box;display:-ms-flexbox;display:flex;-webkit-box-pack:center;-ms-flex-pack:center;justify-content:center;padding:24px 0}@media only screen and (max-width:640px){.todo_f0719dd6{padding:20px}}"}]);
+export const todo = "todo_f0719dd6";
+export const topRow = "topRow_f0719dd6";
+export const todoHeading = "todoHeading_f0719dd6";
+export const todoPivot = "todoPivot_f0719dd6";
+export const todoForm = "todoForm_f0719dd6";
+export const textField = "textField_f0719dd6";
+export const addButton = "addButton_f0719dd6";
+export const todoList = "todoList_f0719dd6";
+export const todoItem = "todoItem_f0719dd6";
+export const itemTaskRow = "itemTaskRow_f0719dd6";
+export const deleteButton = "deleteButton_f0719dd6";
+export const isHidden = "isHidden_f0719dd6";
+export const isCompleted = "isCompleted_f0719dd6";
+export const workingOnItSpinner = "workingOnItSpinner_f0719dd6";
+export const fetchingTasksSpinner = "fetchingTasksSpinner_f0719dd6";
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { Fabric } from 'office-ui-fabric-react/lib/Fabric';
+import { initializeIcons } from 'office-ui-fabric-react/lib/Icons';
+import Todo from './components/Todo';
+import DataProvider from './DataProvider';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import './version';
+
+let _rootDiv: HTMLElement;
+let _dataProvider = new DataProvider();
+
+function start(): void {
+  initializeIcons();
+
+  if (!_rootDiv) {
+    _rootDiv = document.createElement('div');
+    document.body.appendChild(_rootDiv);
+  }
+
+  ReactDOM.render(
+    <Fabric>
+      <Todo dataProvider={_dataProvider} />
+    </Fabric>,
+    _rootDiv,
+  );
+}
+
+// Start the application.
+start();
